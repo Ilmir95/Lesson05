ba53d5440e7898a46207511f24d8192d5b688c1a
Ilmir <Ilmir131313@yandex.ru>
2022-06-06 18:20:06 +0300

    Khafizov_Ilmir_dz_lesson05

---
+++ Khafizov_Ilmir_dz_2.py
@@ -0,0 +1,16 @@
+max_number = 10
+odd_numbers = (n for n in range(1, max_number + 1, 2))
+print(next(odd_numbers))
+
+
+
+
+
+
+
+
+
+# num = []
+# for n in range(1, max_number + 1, 2):
+#     num.append(n)
+# print(num)
